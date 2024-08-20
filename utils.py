c7402f047b491247f6163b3e2b2213b47195e6d2
eitanwagner <eitan.wagner@mail.huji.ac.il>
2024-08-20 15:21:06 +0300

    remove unused

---
+++ utils.py
@@ -0,0 +1,16 @@
+
+import argparse
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--template', type=str, default=" <NP> is a thing")
+    parser.add_argument('--set_num', type=int, default=0)
+
+    parser.add_argument('--load', action="store_true", help="whether to load data")
+    parser.add_argument('--only_make', action="store_true", help="whether to only generate data")
+    parser.add_argument('--base_path', type=str, help="path to project (name included)")
+    parser.add_argument('--cache_dir', type=str, default=None, help="cache directory")
+    parser.add_argument('--seed', type=int, default=None)
+
+    args = parser.parse_args()
+    return args
